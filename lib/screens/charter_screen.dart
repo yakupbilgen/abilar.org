aa73cf8d16fc790d571914223861ed69c7e86054
yakupbilgen <yakupbilgen92@gmail.com>
2021-05-06 19:45:58 +0300

    http.get for charter page

--- lib/screens/charter_screen.dart
+++ lib/screens/charter_screen.dart
@@ -1,6 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
-class CharterScreen extends StatelessWidget {
+class CharterScreen extends StatefulWidget {
+  @override
+  _CharterScreenState createState() => _CharterScreenState();
+}
+
+class _CharterScreenState extends State<CharterScreen> {
+  String _htmlData = '';
+  var _charterUrl = Uri.parse('https://abilar.org/hakkimizda/tuzuk');
+
+  Future _getHttpData() async {
+    await http.get(_charterUrl).then((value) {
+      _htmlData = value.body;
+      debugPrint(_htmlData);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -8,12 +24,21 @@ class CharterScreen extends StatelessWidget {
         centerTitle: true,
         title: Text(
           'Tüzük',
-          style: Theme.of(context).textTheme.headline4,
+          style: Theme.of(context).textTheme.headline5,
         ),
+        actions: [
+          IconButton(
+            onPressed: _getHttpData,
+            icon: Icon(Icons.refresh),
+          )
+        ],
       ),
       body: Center(
-        child: SingleChildScrollView(),
-      ),
+          child: _htmlData.length > 0
+              ? SingleChildScrollView(
+                  child: Text(_htmlData),
+                )
+              : CircularProgressIndicator()),
     );
   }
 }
