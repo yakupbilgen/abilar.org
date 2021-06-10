5c08c72387621f1c1aceea93bf64841b07a44616
yakupbilgen <yakupbilgen92@gmail.com>
2021-06-10 14:23:09 +0300

    new page => inherited_widget.dart

--- lib/widget/slider_widget.dart
+++ lib/widget/slider_widget.dart
@@ -1,1 +1,10 @@
 import 'package:flutter/material.dart';
+
+class MainSlider extends StatelessWidget {
+  const MainSlider({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
